637a62036f1722028902c101b1c49e00b6583401
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-05-23 21:34:52 -0300

    feat: currency domain class

---
+++ server/grails-app/domain/currencyapp/Currency.groovy
@@ -0,0 +1,13 @@
+package currencyapp
+
+class Currency {
+    Integer id
+    String name
+    Double buy
+    Double sell
+    Double variation
+
+    static constraints = {
+        version false
+    }
+}
---
+++ server/src/test/groovy/currencyapp/CurrencySpec.groovy
@@ -0,0 +1,18 @@
+package currencyapp
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class CurrencySpec extends Specification implements DomainUnitTest<Currency> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
