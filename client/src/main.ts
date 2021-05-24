f8692f483f6aced9890615de12ebcfb6100a3dd4
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-05-23 22:29:24 -0300

    chore: add primevue dependencies

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimveVue from 'primevue/config'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimveVue)
+
+app.mount('#app')
+
+
