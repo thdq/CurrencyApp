ab89fdd6c3eb00579591d8350ee19342d97b445f
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-05-24 23:06:25 -0300

    chore: import primevue default theme

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimveVue from 'primevue/config'
 import './index.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
+
 app.use(PrimveVue)
 
 app.mount('#app')
